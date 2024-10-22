1880adb3510040ac2c2d8630a74f233a069ee245
mirdavud9 <mirdavudvm-bp217@code.edu.az>
2024-10-22 10:08:20 +0400

    _initial

---
+++ ListInt.cs
@@ -0,0 +1,110 @@
+﻿namespace ListInt_Struct
+{
+    internal class ListInt
+    {
+        private int[] _arr;
+
+        public int Length
+        {
+            get { return _arr.Length; }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= _arr.Length || index < 0)
+                {
+                    return -1;
+                }
+                return _arr[index];
+            }
+            set
+            {
+                if (index >= 0 && index < _arr.Length)
+                {
+                    _arr[index] = value;
+                }
+            }
+        }
+
+        public ListInt()
+        {
+            _arr = new int[0];
+        }
+        public ListInt(params int[] nums)
+        {
+            _arr = new int[nums.Length];
+            nums.CopyTo(_arr, 0);
+        }
+
+        public ListInt(int length)
+        {
+            _arr = new int[length];
+        }
+
+        public void Add(int value)
+        {
+            Array.Resize(ref _arr, _arr.Length + 1);
+            _arr[_arr.Length - 1] = value;
+        }
+        public override string ToString()
+        {
+            return string.Join(" ", _arr);
+        }
+
+        public int Sum()
+        {
+            int total = 0;
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                total += _arr[i];
+            }
+            return total;
+        }
+
+        public void AddRange(params int[] nums)
+        {
+            int newSize = _arr.Length + nums.Length;
+            Array.Resize(ref _arr, newSize);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                _arr[_arr.Length - nums.Length + i] = nums[i];
+            }
+        }
+
+        public bool Contains(int num)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (_arr[i] == num)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Remove(int num)
+        {
+            int index = Array.IndexOf(_arr, num);
+            if (index == -1) return false;
+
+            for (int i = index; i < _arr.Length - 1; i++)
+            {
+                _arr[i] = _arr[i + 1];
+            }
+            Array.Resize(ref _arr, _arr.Length - 1);
+            return true;
+        }
+
+        public void RemoveRange(params int[] nums)
+        {
+            foreach (var num in nums)
+            {
+                Remove(num);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        ListInt list = new ListInt();
+
+
+        list.Add(14);
+        list.Add(28);
+        list.Add(42);
+
+        Console.WriteLine("Arraya elave edirik ");
+        Console.WriteLine("Arrayin elementleri");
+        list.AddRange(64, 80, 96);
+        Console.WriteLine(list);
+
+        Console.WriteLine("Elementin olub olmadigini yoxlayiriq");
+        Console.WriteLine(list.Contains(14));
+        Console.WriteLine("AArrayin cemi");
+        Console.WriteLine(list.Contains(50)); 
+        Console.WriteLine(list.Sum());
+
+        Console.WriteLine("Her hansisa indexdeki element silinir");
+        list.Remove(64);
+        Console.WriteLine(list);
+
+        Console.WriteLine("Arrayden silinir:");
+        list.RemoveRange(42, 80);
+        Console.WriteLine(list);
+    }
+}
